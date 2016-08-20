23a888161636462e1073749a05f3e0240bf8e1ba
Jenell Pizarro <nellarro@gmail.com>
2016-08-20 09:36:20 -0400

    Fixed Comedy

--- src/Comedy.js
+++ src/Comedy.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
 
 class Comedy extends Component {
-render () {
 
-return <div className='Comedy'>
-<div class="genre"><a name="comedy"><h2>Comedy</h2></a></div>
-<div class="content">
-// get http://www.omdbapi.com/?t={movieName}&plot=short&r=json
-// if "Genre" = "Comedy" or contains "Comedy"
-//CreateElement Div
-<div class="movie"><img src="{this.props.Poster}"></div>
-<h3>{this.props.Title}</h3>
-<p>{this.props.Year}, {this.props.Rated}</p>
-<p>{this.props.Plot}</p>
-<p>{this.props.Awards}</p>
-</div>
+  // get //http://www.omdbapi.com/?t={movieName}&plot=short&r=// json
+  // if "Genre" = "Comedy" or contains "Comedy"
+  // CreateElement Div
+
+  render () {
+    return (
+      <div className='Comedy'>
+        <div className='Genre'><a name='comedy'><h2>Comedy</h2></a>
+        </div>
+        <div className='Content'>
+          <div className='Movie'>
+            <img src= {this.props.Poster} />
+          </div>
+          <h3>{this.props.Title}</h3>
+          <p>{this.props.Year}, {this.props.Rated}</p>
+          <p>{this.props.Plot}</p>
+          <p>{this.props.Awards}</p>
+        </div>
+      </div>
+    )
+  }
 }
-}
+
 export default Comedy
