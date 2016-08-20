229bd3b32ac0349d85df0efff558b924b52d10c4
Jenell Pizarro <nellarro@gmail.com>
2016-08-20 16:54:19 -0400

    Functioning one screen

--- src/Comedy.js
+++ src/Comedy.js
@@ -8,19 +8,7 @@ class Comedy extends Component {
 
   render () {
     return (
-      <div className='Comedy'>
-        <div className='Genre'><a name='comedy'><h2>Comedy</h2></a>
-        </div>
-        <div className='Content'>
-          <div className='Movie'>
-            <img src={this.props.Poster} alt='Move Title'/>
-          </div>
-          <h3>{this.props.Title}</h3>
-          <p>{this.props.Year}, {this.props.Rated}</p>
-          <p>{this.props.Plot}</p>
-          <p>{this.props.Awards}</p>
-        </div>
-      </div>
+      fetch()
     )
   }
 }
