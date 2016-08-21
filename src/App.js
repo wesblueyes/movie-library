d997e939b9752b09f2138732de107eea77a93b39
Whitney Smith <whitney@gallerywrappeddesign.com>
2016-08-21 13:21:20 -0400

    Updated Movie Library

--- src/App.js
+++ src/App.js
@@ -15,20 +15,26 @@ class App extends Component {
     const familyTitles = ['Toy+Story', 'The+Secret+Life+of+Pets', 'Up', 'Spirited+Away']
     return (
       <div className='App'>
-      <h1>Movie Library</h1>
+      <div className='fixed'>
+      <div className='top-nav'>
+      <h1>Movie</h1><img src='http://gallerywrappeddesign.com/wp-content/uploads/2016/08/movie-icon.png'/><h1>Library</h1>
+      </div>
       <Header />
-      <div className='Genre'><a name='horror'><h2>Horror</h2></a></div>
+      </div>
+      <div className='body'>
+      <div className='Genre'><a className='anchor' name='horror'><h2>Horror</h2></a></div>
       <div className='Container'>{horrorTitles.map((argument) =>
           <Horror movieTitle={argument}/>)}</div>
-          <div className='Genre'><a name='comedy'><h2>Comedy</h2></a></div>
+          <div className='Genre'><a className='anchor' name='comedy'><h2>Comedy</h2></a></div>
       <div className='Container'> {comedyTitles.map((argument) =>
           <Comedy movieTitle={argument}/>)}</div>
-          <div className='Genre'><a name='action'><h2>Action</h2></a></div>
+          <div className='Genre'><a className='anchor' name='action'><h2>Action</h2></a></div>
       <div className='Container'>{actionTitles.map((argument) =>
           <Action movieTitle={argument}/>)}</div>
-          <div className='Genre'><a name='family'><h2>Family</h2></a></div>
+          <div className='Genre'><a className='anchor' name='family'><h2>Family</h2></a></div>
       <div className='Container'>{familyTitles.map((argument) =>
           <Family movieTitle={argument}/>)}</div>
+      </div>
       </div>
     )
   }
